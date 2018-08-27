1ebb994267f9b98aa7f9d6c500d3bb6f7e578806
MrFurlan <mrfurlan10@hotmail.com>
2018-08-27 12:18:15 -0300

    Ajuste
    Ajuste campo text

--- app/src/main/java/com/exemple/furlan/authenticationlogin/RegistrationActivity.java
+++ app/src/main/java/com/exemple/furlan/authenticationlogin/RegistrationActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -21,6 +20,7 @@ public class RegistrationActivity extends Activity {
 
     EditText editLogin;
     EditText editSenha;
+
     ProgressDialog progressLogin;
 
     @Override
